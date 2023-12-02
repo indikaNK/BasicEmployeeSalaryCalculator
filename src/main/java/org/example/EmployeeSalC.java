05181b9780b411871bfc51c91eeee6e504be7381
Indika NK <indikank95@gmail.com>
2023-12-02 11:27:30 +0530

    Commit 03 Fixed bug of working hours by using working minutes

--- src/main/java/org/example/EmployeeSalC.java
+++ src/main/java/org/example/EmployeeSalC.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class EmployeeSalC extends JFrame {
@@ -41,11 +42,21 @@ public class EmployeeSalC extends JFrame {
 
 
     //cash flows
+
+//    kiyas - 1100 , 600 + 500
+//    herath - 750 , 500 + 250
+//    kumara - 900 , 500 + 400
     final double earlyCash = 400.00;
-    final double baseCash = 600.00; //for 5 hours
-    final double midCash = 500.00; //for next 3 hours
+    final double baseCash_KIYAS = 600.00; //for 5 hours
+    final double midCash_KIYAS = 500.00; //for next 3 hours
+    final double baseCash_HERATH = 500.00; //for 5 hours
+    final double midCash_HERATH = 250; //for next 3 hours
+    final double baseCash_KUMARA = 500; //for 5 hours
+    final double midCash_KUMARA = 400; //for next 3 hours
     final double OT = 200.00;
 
+//    round up to 2 decimals
+private static final DecimalFormat df = new DecimalFormat("0.00");
     Connection conn;
 
     //meta data with default constructor
@@ -177,12 +188,13 @@ public class EmployeeSalC extends JFrame {
                 System.out.println(in + "-"+out);
 
                 //devide time to 2 parts
-                String [] earlyHour = in.split(":");
-                String [] lateHour = out.split(":");
+                String [] earlyHour = in.split("\\.");
+                String [] lateHour = out.split("\\.");
 //                System.out.println(Arrays.toString(earlyHour));
 
                 //condition to calculate the gross salary
-                if (earlyHour[0].equals("08")){
+
+                if (earlyHour[0].equals("08") || earlyHour[0].equals("8")  ){
                     //if employee come  before 8:10
                     if(Integer.parseInt(earlyHour[1]) >= 0 && Integer.parseInt(earlyHour[1]) <= 10 ){
                         grossSalary = grossSalary + earlyCash;
@@ -190,26 +202,84 @@ public class EmployeeSalC extends JFrame {
                     //basic calculation for first 5 hours
 //
 //                    1) get total hours
-                    int workedHours = Integer.parseInt(lateHour[0])-Integer.parseInt(earlyHour[0]) ;
-                    System.out.println("Worked Hours:"+workedHours);
+                    int workedHours = Integer.parseInt(lateHour[0])-Integer.parseInt(earlyHour[0]) ; //18.00*60 - 8.30*60 = worked mins
+
+                    //early hour is in array of string format in order to do the calculation conversion is a must
+//                    in = 8.07
+                    float workedMins = Float.parseFloat(df.format(Float.parseFloat(out)*60 - Float.parseFloat(in)*60));
+                    System.out.println(workedMins);
 //                    2) Early leave
-                    if(workedHours <= 5){
-                        grossSalary = grossSalary + baseCash/5*workedHours;
-                        System.out.println("Gross Salary for Early leave: "+grossSalary);
-                    }
+                    //employee based salary handling()
+
+                    String selectedEmployee = cmbNames.getSelectedItem().toString().substring(3).toLowerCase();
+                    System.out.println(selectedName);
+
+                    if(selectedEmployee.equalsIgnoreCase("kiyas")){
+                        if(workedHours <= 5){
+                            grossSalary = grossSalary + baseCash_KIYAS/270*workedMins;
+                            System.out.println("Gross Salary for Early leave: "+grossSalary);
+                        }
 //                    2) middle time
-                    else if (workedHours <= 8) {
-                        grossSalary = grossSalary + baseCash/5*5 + midCash/3* (workedHours-5);
-                        System.out.println("Gross Salary for Middle time leave: "+grossSalary);
+                        else if (workedHours <= 8) {
+                            grossSalary = grossSalary + baseCash_KIYAS + midCash_KIYAS/180 * (workedMins-330);
+                            System.out.println("Gross Salary for Middle time leave: "+grossSalary);
 
-                    }
+                        }
 //                    2) rest of hours calculate with OT
-                    else {
-                        //work hours should reduce 9 hours because of lunch hour
-                        grossSalary = grossSalary + baseCash/5*5 + midCash/3*3 + OT*(workedHours-9);
-                        System.out.println("Gross Salary with full time and OT: "+grossSalary);
+                        else {
+                            //work hours should reduce 9 hours because of lunch hour
+                            grossSalary = grossSalary + baseCash_KIYAS + midCash_KIYAS + OT/60*(workedMins- 580);
+                            System.out.println("Gross Salary with full time and OT: "+grossSalary);
+/*
+                            hours	hours	total mins
+                            8.10 - 1.00	4.50	270
+                            8.10 - 5.00	4.50	520 (lunch hor included)
+                            2-5	3	180
+                            5-6	1	60
+                            grossSalary = grossSalary + baseCash_KIYAS/270*workedMins;
+                            grossSalary = grossSalary + baseCash_KIYAS + midCash_KIYAS/180 * (workedMins- 270 + 60) (morning worked hours + lunch_hour));
+                            grossSalary = grossSalary + baseCash_KIYAS + midCash_KIYAS + OT/60*(workedMins-9*60);
 
+*/
+                        }
+                    } else if (selectedEmployee.equalsIgnoreCase("herath")) {
+                        if(workedHours <= 5){
+                            grossSalary = grossSalary + baseCash_HERATH/270*workedMins;
+                            System.out.println("Gross Salary for Early leave: "+grossSalary);
+                        }
+//                    2) middle time
+                        else if (workedHours <= 8) {
+                            grossSalary = grossSalary + baseCash_HERATH + midCash_HERATH/180* (workedMins-330);
+                            System.out.println("Gross Salary for Middle time leave: "+grossSalary);
+
+                        }
+//                    2) rest of hours calculate with OT
+                        else {
+                            //work hours should reduce 9 hours because of lunch hour
+                            grossSalary = grossSalary + baseCash_HERATH + midCash_HERATH + OT/60*(workedMins- 580);
+                            System.out.println("Gross Salary with full time and OT: "+grossSalary);
+
+                        }
+                    } else if (selectedEmployee.equalsIgnoreCase("kumara")) {
+                        if(workedHours <= 5){
+                            grossSalary = grossSalary + baseCash_KUMARA/270*workedMins;
+                            System.out.println("Gross Salary for Early leave: "+grossSalary);
+                        }
+//                    2) middle time
+                        else if (workedHours <= 8) {
+                            grossSalary = grossSalary + baseCash_KUMARA + midCash_KUMARA/180*(workedMins - 330);
+                            System.out.println("Gross Salary for Middle time leave: "+grossSalary);
+
+                        }
+//                    2) rest of hours calculate with OT
+                        else {
+                            //work hours should reduce 9 hours because of lunch hour
+                            grossSalary = grossSalary + baseCash_KUMARA + midCash_KUMARA + OT/60*(workedMins- 580);
+                            System.out.println("Gross Salary with full time and OT: "+grossSalary);
+
+                        }
                     }
+
 
                 }
 
@@ -217,7 +287,7 @@ public class EmployeeSalC extends JFrame {
 
             System.out.println("Gross Salary = "+grossSalary);
             if(grossSalary > 0){
-                txtGrossSalary.setText(String.valueOf(grossSalary));
+                txtGrossSalary.setText(String.valueOf(df.format(grossSalary)));
             }
 
 
@@ -243,23 +313,25 @@ rs.close();
 
     private void clearAll() {
 //        JOptionPane.showMessageDialog(EmployeeSalC.this,"Hi hi");
-        txtEname.setText(null);
+//        txtEname.setText(null);
         txtGrossSalary.setText(null);
         totalSalary.setText(null);
         txtEtf.setText(null);
         txtLoans.setText(null);
         txtAdvance.setText(null);
         txtNetsalary.setText(null);
+        grossSalary = 0;
+
     }
 
     private void calculate(){
         totalSal = grossSalary + basicSalary;
 
-        totalSalary.setText(String.valueOf(totalSal));
+        totalSalary.setText(String.valueOf(df.format(totalSal)));
 
         //etf deductions
         etfDeductedBal = totalSal - ETF;
-        txtEtf.setText(String.valueOf(etfDeductedBal));
+        txtEtf.setText(String.valueOf(df.format(etfDeductedBal)));
 
         //calculate netSal
         netSal = etfDeductedBal;
@@ -284,7 +356,7 @@ rs.close();
 
             }
         }
-        txtNetsalary.setText(String.valueOf(netSal));
+        txtNetsalary.setText(String.valueOf(df.format(netSal)));
     }
 
     public static void main(String[] args) {
